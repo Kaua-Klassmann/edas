0b50f225798bca0494aa5d2c6b81d8507d17a95e
Kauã Klassmann <131408936+Kaua-Klassmann@users.noreply.github.com>
2023-10-04 19:56:47 -0300

    Delete Back-end directory

--- Back-end/src/config/auth.js
+++
@@ -1,4 +0,0 @@
-export default {
-    secret: '5d7cea8dc670146029bdd6ab1479ad0e7700c0c1',
-    expiresIn: '1d',
-};
--- Back-end/src/server.js
+++
@@ -1,3 +0,0 @@
-import server from "./App.js";
-const port = process.env.PORT || 3000;
-server.listen(port, () => console.log("Servidor rodando:", port));
